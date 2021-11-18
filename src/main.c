b4425e07e0b933ece08bd5af5a4e0ad79b63cc9b
Sam Belliveau <sam.belliveau@gmail.com>
2021-11-18 12:33:04 -0500

    Tried to handle spaces

--- src/main.c
+++ src/main.c
@@ -5,6 +5,6 @@
 
 int main(void)
 {
-    struct shell_command* command = shell_command_create("ls ./this_folder/;echo Hello World");
+    struct shell_command* command = shell_command_create("ls ./this_folder/; echo Hello World");
     shell_command_print(0, command);
 }
--- src/shell_command.c
+++ src/shell_command.c
@@ -3,7 +3,7 @@
 struct shell_command* shell_command_create(char *user_line)
 {
     int size;
-    char* i, buf;
+    char *i, *buf;
     struct shell_command* command = calloc(1, sizeof(struct shell_command));
     
     command->argc = 0;
@@ -16,11 +16,22 @@ struct shell_command* shell_command_create(char *user_line)
         || *i == SH_COMMAND_END 
         || *i == '\0')
         {
+            // See how long buffer is
             size = i - user_line;
+            
+            // Get rid of empty space
+            // if(size == 0) 
+            // {
+            //     ++user_line;
+            //     break;
+            // }
+            
+            // Copy buffer into command
             buf = calloc(size + 1, sizeof(char));
             strncpy(buf, user_line, size);
             command->argv[command->argc++] = buf;
             
+            // Based on type of delimiter, handle it differently
             if(*i == SH_DELIMITER)
             {
                 command->next_command = shell_command_create(i + 1);
