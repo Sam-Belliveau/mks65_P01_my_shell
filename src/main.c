6e18ae65004909f17b187056a41ff7b09ef11df6
Sam Belliveau <sam.belliveau@gmail.com>
2021-11-28 16:51:25 -0500

    Move variable

--- src/main.c
+++ src/main.c
@@ -5,13 +5,15 @@
 
 int main(void)
 {
+    struct shell_command* user_command;
+
     printf("Welcome to " SH_PROGRAM_NAME " " SH_VERSION_NO "!\n");
 
     while(1)
     {
         shell_print_header();
 
-        struct shell_command* user_command = shell_get_user_line();
+        user_command = shell_get_user_line();
         shell_execute_commands(user_command);
         shell_command_free(user_command);
     }
